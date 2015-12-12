88315b6fb2d30285400e4882c8715203637b4a99
Ricardo Jesus <rj.bcjesus@gmail.com>
2015-12-12 13:00:54 +0000

    add_emails.py
    Former-commit-id: 6e2a544527156e2dee917b81b8a77c05f11317cb

---
+++ application/ours_dataset/add_emails.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+
+def add_files(s_dir):
+    files = os.listdir(s_dir)
+    f_name = '00000.txt'
+    for f in files:
+        f_name = max(f, f_name)
+    f_name = int(f_name.replace('.txt', ''))
+    while True:
+        f_name += 1
+        s_from = ('FROM: ' + raw_input('From: ')).lower()
+        s_body = raw_input('Body: ').lower()
+        with open(os.path.join(s_dir, str(f_name).zfill(5) + '.txt'), 'w') as f_out:
+            f_out.write(s_from + '\n')
+            f_out.write(s_body)
+
+
+if __name__ == '__main__':
+    opt = input('Add [Spam = 0] or [Ham = 1]: ')
+    if opt:
+        add_files('ham/')
+    else:
+        add_files('spam/')
